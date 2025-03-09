eb15d45b6d21915ec0380313f93459ec1e6e2ba0
eunjoong kim <66717979+ejinhvn-0112@users.noreply.github.com>
2025-03-09 14:27:50 +0900

    Initial Commit

---
+++ ChattingApp/ChattingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingAppApp.swift
+//  ChattingApp
+//
+//  Created by 김은중 on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ChattingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChattingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChattingApp
+//
+//  Created by 김은중 on 3/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
